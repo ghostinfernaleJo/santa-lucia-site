bcead4aec2002b15a9817dfbb56111cefff3d960
Santa Lucia Dev <dev@complexesantalucia.com>
2026-09-19 13:20:09 +0100

    Bust OMTLAND campaign stylesheet cache

--- wp-content/plugins/sl-agences-elementor/includes/omtland-bonus-campaign.php
+++ wp-content/plugins/sl-agences-elementor/includes/omtland-bonus-campaign.php
@@ -75,7 +75,7 @@ function sl_omtland_campaign_assets() {
 	if ( ! is_page( 'bonus-omtland-odza' ) ) {
 		return;
 	}
-	wp_enqueue_style( 'sl-omtland-bonus', SL_AGENCES_URL . 'assets/css/omtland-bonus.css', array(), '1.0.1' );
+	wp_enqueue_style( 'sl-omtland-bonus', SL_AGENCES_URL . 'assets/css/omtland-bonus-v2.css', array(), '1.0.2' );
 }
 add_action( 'wp_enqueue_scripts', 'sl_omtland_campaign_assets', 110 );
 
